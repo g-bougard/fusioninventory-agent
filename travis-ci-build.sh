e2fccf60fa1f7d01ec7b692eadde23932986c01e
Guillaume Bougard <gbougard@teclib.com>
2015-06-24 09:17:07 +0200

    Travis test

--- travis-ci-build.sh
+++ travis-ci-build.sh
@@ -2,9 +2,25 @@
 
 set -e
 
-perlbrew list
+echo "Install more perl lib..."
+cpanm --quiet --notest Net::NBName
+cpanm --quiet --notest Parse::EDID
+echo
 
+echo "List of perl available versions:"
+perlbrew list
+echo
+
+echo "Git status:"
 git status
+echo
+
+echo "System info:"
+ip addr
+echo
+
+echo "Let's go !!!"
+echo
 
 perl Makefile.PL
 
