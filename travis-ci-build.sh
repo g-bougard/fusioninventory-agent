7836b23f52360ee0cf8cf05db01365c1312fee5a
Guillaume Bougard <gbougard@teclib.com>
2015-06-28 16:10:59 +0200

    Cleanup travic-ci build script

--- travis-ci-build.sh
+++ travis-ci-build.sh
@@ -2,19 +2,12 @@
 
 set -e
 
-echo "Installed packages:"
-dpkg -l
-
 echo "List of perl available versions:"
 perlbrew list
 echo
 
 echo "Git status:"
 git status
-echo
-
-echo "System info:"
-ip addr
 echo
 
 echo "Let's go !!!"
